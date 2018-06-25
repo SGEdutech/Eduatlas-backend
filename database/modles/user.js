677d537b752d5fc19af506d79bc01047c2ec274f
Ashish Kuamr <entropy.ash@gmail.com>
2018-06-25 12:16:52 +0530

    Update user schema

--- database/modles/user.js
+++ database/modles/user.js
@@ -7,16 +7,31 @@ const UserSchema = new Schema({
     eventsOwned: [String],
     tuitionsOwned: [String],
     schoolsOwned: [String],
+    primaryRole: String, // Institute, student, parent
     isMale: Boolean,
-    address: String,  //you knwo the drill
-    name: String,
-    email: String,  //should be unique also alternate
+    addressLine1: String,
+    addressLine2: String,
+    city: String,
+    district: String,
+    state: String,
+    country: String,
+    pin: Number,
+    firstName: String,
+    middleName: String,
+    lastName: String,
+    primaryEmail: String,  //should be unique also alternate
+    secondaryEmail: String,
     phone: Number,
     img_profilePicPath: String,
     dateOfBirth: Date,
     goingEvents: [String],
     mayBeGoingEvents: [String],
     schoolStuding: String,  //history
+    fbLink: String,
+    twitterLink: String,
+    youtubeLink: String,
+    instaLink: String,
+    linkedinLink: String,
     bookmarks: [String]
 });
 //privacy
