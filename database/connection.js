f4267406298ddb6f0664a6da2eb407272ff2044f
Ashish Kumar <entropy.ash@gmail.com>
2018-07-16 12:35:08 +0530

    Add: function to clean up database

--- database/connection.js
+++ database/connection.js
@@ -2,5 +2,26 @@ const mongoose = require('mongoose');
 const uri = require('../config').MONGO.URI;
 
 mongoose.connect(uri)
-    .then(() => console.log('DB\'s connected niggas!!!'))
+    .then(() => {
+        console.log('DB\'s Connected');
+        cleanUp();
+    })
     .catch(err => console.error(err));
+
+
+const Tuition = require('./modles/tuition')
+
+function cleanUp() {
+    console.log('Running cleanup');
+    Tuition.find()
+        .then(data => {
+        data.forEach(item => {
+            if (item.dayAndTimeOfOperation && item.dayAndTimeOfOperation.length !== 0 && item.dayAndTimeOfOperation[0].day === undefined) {
+                item.dayAndTimeOfOperation = [];
+                item.save();
+            }
+        });
+            console.log('Cleanup completed');
+    })
+        .catch(err => console.error(err));
+}
