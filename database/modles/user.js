3282a515141a18b86e3d2926fe0371cf70c91287
Ashish Kumar <entropy.ash@gmail.com>
2018-08-08 16:08:50 +0530

    Bookmarks must be unique

--- database/modles/user.js
+++ database/modles/user.js
@@ -38,10 +38,10 @@ const UserSchema = new Schema({
     youtubeLink: String,
     instaLink: String,
     linkedinLink: String,
-    bookmarkTuitions: [String],
-    bookmarkSchools: [String],
-    bookmarkEvents: [String],
-    bookmarkBlogs: [String],
+    bookmarkTuitions: [{type: String, unique: true}],
+    bookmarkSchools: [{type: String, unique: true}],
+    bookmarkEvents: [{type: String, unique: true}],
+    bookmarkBlogs: [{type: String, unique: true}]
 });
 // Privacy
 
