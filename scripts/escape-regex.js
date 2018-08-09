e2996245abe04127c5799ed747a80a9aff1d0f68
Ashish Kumar <entropy.ash@gmail.com>
2018-08-09 12:27:19 +0530

    Make function escape regex return if argument is empty

--- scripts/escape-regex.js
+++ scripts/escape-regex.js
@@ -1,3 +1,7 @@
 module.exports = text => {
+    if (text === undefined) {
+        console.info('No text provided');
+        return;
+    }
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 };
