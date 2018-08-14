40d65c21888018189dc39e4352cda2ca7029956a
Ashish Kumar <entropy.ash@gmail.com>
2018-08-14 12:17:08 +0530

    Change status code again for unknown host

--- scripts/redirect-unknown-host-middleware.js
+++ scripts/redirect-unknown-host-middleware.js
@@ -1,7 +1,7 @@
 function redirectUnknownHostMiddleware(req, res, next) {
     // localhost is for dev purpose
     if (/eduatlas.com$/.test(req.get('host')) === false && req.get('host').startsWith('localhost') === false) {
-        res.status(301).redirect('https://eduatlas.com');
+        res.status(404).redirect('https://eduatlas.com');
         return;
     }
     next();
