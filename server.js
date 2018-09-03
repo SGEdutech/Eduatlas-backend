2d5cc88cf7e452ce514f26a1394935ca88035296
Ashish Kumar <entropy.ash@gmail.com>
2018-09-03 12:06:52 +0530

    Remove database from this repository

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
-const PORT = require('./config').SERVER.PORT;
+const PORT = require('./config')
+	.SERVER.PORT;
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
-const keys = require('./oauth/_config').keys;
+const keys = require('./oauth/_config')
+	.keys;
 const {
-    eventPicsMiddleware,
-    schoolPicsMiddleware,
-    tuitionPicsMiddleware,
-    userCoverPicMiddleware,
-    solutionPdfMiddleware
+	eventPicsMiddleware,
+	schoolPicsMiddleware,
+	tuitionPicsMiddleware,
+	userCoverPicMiddleware,
+	solutionPdfMiddleware
 } = require('./storage-engine');
-const {nestingMiddleware} = require('./scripts/nesting');
-const {passwordHashMiddleware} = require('./scripts/hash-password');
+const { nestingMiddleware } = require('./scripts/nesting');
+const { passwordHashMiddleware } = require('./scripts/hash-password');
 const redirectUnknownHostMiddleware = require('./scripts/redirect-unknown-host-middleware');
 const sanitizeDemandsMiddleware = require('./scripts/sanatize-demands');
 require('./oauth/local');
@@ -23,15 +25,15 @@ require('./oauth/facebook');
 require('./database/connection');
 
 const routes = {
-    blog: require('./database/api/blog'),
-    event: require('./database/api/event'),
-    school: require('./database/api/school'),
-    tuition: require('./database/api/tuition'),
-    user: require('./database/api/user'),
-    issue: require('./database/api/issue'),
-    auth: require('./oauth/auth_routes'),
-    forgot: require('./oauth/forgot'),
-    solution: require('./database/api/solution')
+	blog: require('./database/api/blog'),
+	event: require('./database/api/event'),
+	school: require('./database/api/school'),
+	tuition: require('./database/api/tuition'),
+	user: require('./database/api/user'),
+	issue: require('./database/api/issue'),
+	auth: require('./oauth/auth_routes'),
+	forgot: require('./oauth/forgot'),
+	solution: require('./database/api/solution')
 };
 
 const app = express();
@@ -41,9 +43,9 @@ app.use(cors());
 app.use(redirectUnknownHostMiddleware);
 
 app.use(session({
-    secret: keys.CookieKey,
-    cookie: {maxAge: 7 * 24 * 60 * 60 * 1000},
-    maxAge: Date.now() + (7 * 86400 * 1000)
+	secret: keys.CookieKey,
+	cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+	maxAge: Date.now() + (7 * 86400 * 1000)
 }));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -51,7 +53,7 @@ app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(helmet());
 
@@ -72,8 +74,7 @@ app.get('/*', sanitizeDemandsMiddleware);
 app.use(nestingMiddleware, passwordHashMiddleware);
 
 app.use((req, res, next) => {
-    console.log(req.body);
-    next();
+	next();
 });
 
 app.use('/blog', routes.blog);
@@ -86,6 +87,7 @@ app.use('/auth', routes.auth);
 app.use('/forgot', routes.forgot);
 app.use('/slept-through-classs', routes.solution);
 
-app.get('/*', (req, res) => res.status(404).sendFile(path.join(__dirname, 'public', 'error-page.html')));
+app.get('/*', (req, res) => res.status(404)
+	.sendFile(path.join(__dirname, 'public', 'error-page.html')));
 
 app.listen(PORT, () => console.log(`Yo dawg! Server's at http://localhost:${PORT}`));
